b60c03918d3a1e8dc28ff2bf5ae3d91765719f4c
dnsdudrla97 <dnsdudrla97@naver.com>
2020-03-26 22:24:38 +0900

    file_lsp

---
+++ file_basic/fseek.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+
+
+
+// 파일 생성 후 내용 추가
+static int create_file(void)
+{	
+	FILE *fp;
+	if (!(fp = fopen("datafile", "w"))) {
+		printf("fopen() fail\n");
+		return (-1);
+	}
+	
+	// 파일 포지션 값 읽어오기
+	printf("after fopen(). offset=%ld\n",ftell(fp));
+	fputs("hello World\n", fp);
+	fputs("hello World!\n", fp);
+	fputs("hello World!!\n", fp);
+	printf("middle fopen(). offset=%ld\n",ftell(fp));
+	fputs("hello World!!!\n", fp);
+	fputs("hello World!!!!\n", fp);
+	printf("before fclose(). offset=%ld\n", ftell(fp));
+	printf("---------------------------\n");
+
+	fclose(fp);
+	return (0);
+}
+
+static int read_file(void)
+{
+	FILE *fp;
+	char buf[1024];	// fgets() 
+
+	// 읽고 쓰기 권한
+	if (!(fp = fopen("datafile", "r+"))) {
+		printf("foepn() fail\n");
+		return (-1);
+	}
+
+	printf("after fopen(). offset=%ld\n", ftell(fp));
+	memset(buf, 0, sizeof(buf));
+	fgets(buf, sizeof(buf), fp);
+	printf("after fgets(). offset=%ld\n", ftell(fp));
+	
+	fseek(fp, 0, SEEK_END);	// 파일의 끝을 기준으로 옵셋이 0
+	printf("after fseek(). offset=%ld\n", ftell(fp));
+	fputs("final\n", fp);
+	printf("before fclose(). offset=%ld\n", ftell(fp));
+	printf("-------------------------\n");
+
+	
+	fclose(fp);
+	return (0);
+}
+
+
+int main(int argc, char** argv)
+{
+	create_file();
+	
+	read_file();
+
+	return (0);
+}
